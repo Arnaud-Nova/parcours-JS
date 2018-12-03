34559d218fbb3376b7e700d238fc15308221aa80
Arnaud-Nova <calmelsarnaud@gmail.com>
2018-12-03 09:50:58 +0100

    calculatrice exo3 ok léger bug non bloquant à la première saisie, à traiter en fin de parcours

--- js/exo3.js
+++ js/exo3.js
@@ -6,3 +6,35 @@
  *      on additionne les deux nombres.
  * 3. On affiche le résultat dans #result
  */
+var calcul = {
+  init: function() {
+    var firstNumber = document.querySelector('#input1');
+    var secondNumber = document.querySelector('#input2');
+    firstNumber.addEventListener('blur', calcul.numberOne);
+    secondNumber.addEventListener('blur', calcul.numberTwo);
+
+  },
+
+  numberOne: function(event) {
+    calcul.one = event.target.value;
+    if (calcul.one !== '' && calcul.two !== '') {
+      calcul.result();
+    }
+  },
+
+  numberTwo: function(event) {
+    calcul.two = event.target.value;
+    if (calcul.one !== '' && calcul.two !== '') {
+      calcul.result();
+    }
+  },
+
+  result: function() {
+    document.querySelector('#result').textContent = '';
+    var result = Number(calcul.one) + Number(calcul.two);
+    document.querySelector('#result').textContent = 'Le résultat de la somme est ' + result;
+  }
+
+};
+
+calcul.init();
