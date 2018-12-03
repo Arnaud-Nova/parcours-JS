892bb391fcb9a547d83ec8d28459349abf712b32
Arnaud-Nova <calmelsarnaud@gmail.com>
2018-12-03 09:43:06 +0100

    calculatrice exo2 ok

--- js/exo2.js
+++ js/exo2.js
@@ -4,3 +4,32 @@
  * 2. À la soumission du formulaire, on additionne les deux nombres,
  * 3. On affiche le résultat dans #result.
  */
+var calcul = {
+  init: function() {
+    var firstNumber = document.querySelector('#input1');
+    var secondNumber = document.querySelector('#input2');
+    var sum = document.querySelector('#container > button');
+    firstNumber.addEventListener('blur', calcul.numberOne);
+    secondNumber.addEventListener('blur', calcul.numberTwo);
+    sum.addEventListener('click', calcul.result);
+
+  },
+
+  numberOne: function(event) {
+    calcul.one = event.target.value;
+  },
+
+  numberTwo: function(event) {
+    calcul.two = event.target.value;
+  },
+
+  result: function(event) {
+    document.querySelector('#result').textContent = '';
+    event.preventDefault();
+    var result = Number(calcul.one) + Number(calcul.two);
+    document.querySelector('#result').textContent = 'Le résultat de la somme est ' + result;
+  }
+
+};
+
+calcul.init();
