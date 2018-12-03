f7a7454a994913f548fbfe68f52d390c1790c06b
Arnaud-Nova <calmelsarnaud@gmail.com>
2018-12-03 18:26:40 +0100

    exo 4 ok

--- js/exo4.js
+++ js/exo4.js
@@ -7,3 +7,22 @@
  * Astuce : on peut avoir un objet de date en faisant : var now = new Date();
  * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
  */
+function clock() {
+  var now = new Date();
+  var hours = twoNumbers(now.getHours());
+  var minutes = twoNumbers(now.getMinutes());
+  var seconds = twoNumbers(now.getSeconds());
+  var div = document.querySelector('#container');
+  div.textContent = hours + 'h ' + minutes + 'm ' + seconds + 's';
+  setTimeout(clock, 1000);
+}
+
+function twoNumbers(val) {
+  if (parseInt(val) < 10) {
+    val = '0' + val;
+  }
+  return val;
+}
+
+
+clock();
